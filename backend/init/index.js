e1af0a0720730d488619feddc69c2a3eabff11ef
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2025-12-26 10:15:48 +0545

    feat(auth): implement owner for listings

--- backend/init/index.js
+++ backend/init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "694a7097a9ea801493908ace",
+  }));
   await Listing.insertMany(initData.data);
   console.log("Data was initialized");
 };
--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -36,11 +36,14 @@ router.get(
   "/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+      .populate("reviews")
+      .populate("owner");
     if (!listing) {
       req.flash("error", "Listing doesn't exists!!!");
       return res.redirect("/listings");
     }
+    console.log(listing)
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -52,7 +55,7 @@ router.post(
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
-
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created");
     res.redirect("/listings");
