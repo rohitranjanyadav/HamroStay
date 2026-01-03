26cf618ecda8b9f89656da03c24a4a0d3f1ad3ff
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-01-03 16:05:07 +0545

    cloudinary configured

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -6,14 +6,21 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingController.createListing)
+  // );
+  .post(upload.single("listing[image]"), (req, res) => {
+    res.send(req.file);
+  });
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
